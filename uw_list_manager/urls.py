5b18ac764b37a316c94f70e44ca8db87dea32088
mike seibel <mikes@washington.edu>
2024-04-02 10:47:30 -0700

    urls namespace

--- uw_list_manager/urls.py
+++ uw_list_manager/urls.py
@@ -5,7 +5,8 @@ from django.urls import re_path
 from uw_list_manager.views.api.list_exists import ListExistsView
 
 
+app_name="uw_list_manager"
 urlpatterns = [
     re_path(r'^api/v1/list/(?P<list_name>[\w\-]+)/exists$',
-            ListExistsView.as_view(), name='list_name'),
+            ListExistsView.as_view(), name='list_exists'),
 ]
